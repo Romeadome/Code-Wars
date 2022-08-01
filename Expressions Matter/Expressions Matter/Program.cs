b70e99f03e8d3e406d24fa1834a50773b0842026
Roman Love <romeloves2program@gmail.com>
2022-07-31 22:41:19 -0400

    Kata's Completed

---
+++ Expressions Matter/Expressions Matter/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+
+EXAMPLE: 
+
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+    1 * (2 + 3) = 5
+    1 * 2 * 3 = 6
+    1 + 2 * 3 = 7
+    (1 + 2) * 3 = 9
+
+*/
+
+namespace Expressions_Matter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine($"Test Case: 1, Expected: 6 \n\tResult: {ExpressionsMatter(2, 1, 2)}");
+            Console.WriteLine($"Test Case: 2, Expected: 3 \n\tResult: {ExpressionsMatter(1, 1, 1)}");
+            Console.WriteLine($"Test Case: 3, Expected: 4 \n\tResult: {ExpressionsMatter(2, 1, 1)}");
+            Console.WriteLine($"Test Case: 4, Expected: 9 \n\tResult: {ExpressionsMatter(1, 2, 3)}");
+            Console.WriteLine($"Test Case: 5, Expected: 5 \n\tResult: {ExpressionsMatter(1, 3, 1)}");
+            Console.WriteLine($"Test Case: 6, Expected: 8 \n\tResult: {ExpressionsMatter(2, 2, 2)}");
+            Console.ReadLine();
+           
+        }
+
+        public static int ExpressionsMatter(int a, int b, int c)
+        {
+            int[] sum = new int[6];
+
+            sum[0] = a * b * c;
+            sum[1] = a + b + c;
+            sum[2] = a * b + c;
+            sum[3] = a * (b + c);
+            sum[4] = (a + b) * c;
+            sum[5] = a + b * c;
+
+
+            return sum.Max();
+        }
+    }
+}
---
+++ Sort and Star/Sort and Star/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+*/
+namespace Sort_and_Star
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TwoSort( new string[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" }));
+            Console.WriteLine(TwoSort(new string[] { "car", "apple", "banana", "Car" }));
+            Console.ReadLine();
+        }
+
+        public static string TwoSort(string[] s)
+        {
+            //string[] sortedString = s;
+            Array.Sort(s, StringComparer.Ordinal);
+            string sortedStarString = "";
+
+            for (int i = 0; i < s[0].Length; i++)
+            {
+
+                sortedStarString += $"{s[0][i]}***";
+            }
+            sortedStarString = sortedStarString.Remove(sortedStarString.Length - 3);
+            return sortedStarString;
+        }
+    }
+}
