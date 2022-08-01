1a5619266d51bd6802a0ea80529a0e0ff8828ebf
Roman Love <romeloves2program@gmail.com>
2022-07-31 23:04:26 -0400

    Added Completed Kata

---
+++ Sum The Strings/Sum The Strings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+
+Notes:
+
+    If either input is an empty string, consider it as zero.
+
+    Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+*/
+
+namespace Sum_The_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringsSum("4","5"));
+            Console.ReadLine();
+
+        }
+        public static string StringsSum(string s1, string s2)
+        {
+            if (s1 == "")
+            {
+                s1 = "0";
+            }
+            if (s2 == "")
+            {
+                s2 = "0";
+            }
+            int n1 = Convert.ToInt32(s1);
+            int n2 = Convert.ToInt32(s2);
+            int sum = n1 + n2;
+
+            return sum.ToString();
+        }
+
+    }
+}
