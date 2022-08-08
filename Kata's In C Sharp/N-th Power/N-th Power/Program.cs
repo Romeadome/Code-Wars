38e845a38b67aa78456d081fbc7b42ffe43f574f
Roman Love <romeloves2program@gmail.com>
2022-08-08 15:58:18 -0400

    Solved additional kata problems
    Worked on additional Kata that utilizes finding the sum of an object array that contains values of different data types, and a kata that finds the N-th power of the element in the array with the index N

---
+++ Kata's In C Sharp/N-th Power/N-th Power/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ This kata is from check py.checkio.org
+
+You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+Let's look at a few examples:
+
+    array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+    array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
+*/
+namespace N_th_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Output should be: 9\n Result: {0}",Index (new int[] {1,2,3,4}, 2));
+            Console.WriteLine("Output should be: 1000000\n Result: {0} ",Index(new int[] { 1, 3, 10, 100 }, 3));
+            Console.ReadLine();
+        }
+        public static double Index(int[] array, int n)
+        {
+            if (n >= array.Length)
+            {
+                return -1;
+            }
+            return Math.Pow(array[n], n);
+
+        }
+    }
+}
---
+++ Kata's In C Sharp/Sum Mixed Array/Sum Mixed Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+namespace Sum_Mixed_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Expected Result : 22 \nResult: {0}", SumMix(new object[] { 9, 3, "7", "3" }));
+            Console.WriteLine("Expected Result : 42 \nResult: {0}", SumMix(new object[] { "5", "0", 9, 3, 2, 1, "9", 6, 7 }));
+            Console.WriteLine("Expected Result : 41 \nResult: {0}", SumMix(new object[] { "3", 6, 6, 0, "5", 8, 5, "6", 2, "0" }));
+            Console.ReadLine();
+        }
+
+        public static int SumMix(object[] x)
+        {
+            int[] newObj = x.Select(Convert.ToInt32).ToArray();
+            return newObj.Sum();
+        }
+    }
+}
